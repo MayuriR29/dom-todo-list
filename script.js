f2c616904176d003e2f58f9bcb2557e500c96613
MayuriR29 <mayuri.gr@gmail.com>
2018-06-09 18:06:07 +0800

    Made changes for addEventListener

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ const tasks = [
 	'give feedback',
 ];
 function appendtask(element) {
-	let varClick=true;
-	const varLi = document.createElement('li');
+	const varLi = document.createElement('li'); //
 	document.querySelector('ul').appendChild(varLi);
 	varLi.textContent = element;
+	//first argument of forEach function is the value of array at index i
 	varLi.addEventListener('click', function (event) {
-		if (varClick){
-			varLi.classList.add('done');
-			varClick=false;
-		}
-		else{
-			varLi.classList.remove('done');
-			varClick=true;
-		}
-
+	varLi.classList.toggle("done");
+	/*When only one argument is present: 
+	Toggle class value; i.e., if class exists 
+	then remove it and return false, if not, then add it and return true. */
+	//
 	});
 
 }
 tasks.forEach(appendtask);
+//### Adding more To-Do items
+function addTodo(){
+	const varInput=document.createElement('input');
+	const varButton=document.createElement('button');
+	document.querySelector('body').appendChild(varInput);
+	document.querySelector('body').appendChild(varButton);
+	varButton.textContent='Add Todo';
+	
+};
+addTodo();
+document.querySelector('button').addEventListener('click',function(event){
+	const valueTodo=document.querySelector('input').value;
+	const varLi = document.createElement('li'); //
+	document.querySelector('ul').appendChild(varLi);
+	varLi.textContent = valueTodo;
+	document.querySelector('input').value='';
+});
 
-const varLi = document.querySelector('li');
 
 
 
